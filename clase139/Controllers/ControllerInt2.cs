d27cfa51af07da3b62725f14f851fba6cd0b3db8
Franco Mariani <franco-mariani@hotmail.com>
2022-09-15 17:09:32 -0300

    Se creo ControllerInt2 y se agrego un comentario

---
+++ clase139/Controllers/ControllerInt2.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace clase139.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ControllerInt2 : ControllerBase
+    {
+        //comentario
+    }
+}
+
