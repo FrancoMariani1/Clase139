d7ed4c43839f5c79551d9a69754db2c3214ace74
brendaahumada <110502311+brendaahumada@users.noreply.github.com>
2022-09-16 22:17:55 -0300

    Aca esta el controllerInt3

---
+++ clase139/Controllers/ControllerInt3.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clase139.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ControllerInt3 : Controller
+    {
+      //comentario3
+    }
+}
